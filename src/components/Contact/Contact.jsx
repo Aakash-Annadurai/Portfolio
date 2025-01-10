21d90660a1e4d51385c4b412108642c3081feefc
Aakash Annadurai <aakashannadurai7@gmail.com>
2025-01-10 12:21:07 +0530

    updated forms

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,42 @@
 import React from "react";
 import "./contact.css";
 import separator from "./assets/separator.png";
+import Swal from "sweetalert2";
 
 export default function Contact() {
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const ACCESS_KEY = import.meta.env.VITE_ACCESS_KEY;
+
+    formData.append("access_key", ACCESS_KEY);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      Swal.fire({
+        title: "Success!",
+        text: "Form sent successfully!",
+        icon: "success",
+        confirmButtonColor: "#1a1a1a",
+      });
+      event.target.reset();
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Something went wrong!, Try again.",
+        confirmButtonColor: "#1a1a1a",
+      });
+    }
+  };
+
   return (
     <>
       <div className="Contact" id="contactMe">
@@ -13,7 +47,7 @@ export default function Contact() {
           <p>"Let's Connect and Create Something Amazing Together!"</p>
         </div>
         <img src={separator} alt="separator" />
-        <form>
+        <form onSubmit={onSubmit}>
           <div className="inputfields">
             <input
               type="text"
@@ -44,7 +78,9 @@ export default function Contact() {
               required
             />
           </div>
-          <button className="btn submit">SUBMIT</button>
+          <button className="btn submit" type="submit">
+            SUBMIT
+          </button>
         </form>
       </div>
     </>
